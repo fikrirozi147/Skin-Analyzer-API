64e2104b0cd14a38b2be3dc89ad6c8c691b882dc
code:nemesis <78771209+fikrirozi147@users.noreply.github.com>
2023-12-26 19:48:26 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 // Middleware for parsing JSON data
 app.use(bodyParser.json());
